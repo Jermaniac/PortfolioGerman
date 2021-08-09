f4d492a4268e416a7aa238beb49675255bd3641c
Jermaniac <stalin_bnk@hotmail.com>
2021-08-10 00:28:22 +0200

    create footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Footer from "./components/footer/Footer";
+
 function App() {
   return (
     <div className="App">
-      EY
+      <Footer></Footer>
+      This is a link to my expression recognition using Machine Learning
+      <a href="https://expression-test.vercel.app/">
+        My amazing project
+      </a>
     </div>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,7 @@
+const Footer = () => (
+    <div className="footer">
+      <p>This is some content in sticky footer</p>
+    </div>
+  );
+
+export default Footer;
