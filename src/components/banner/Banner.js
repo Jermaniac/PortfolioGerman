5ea25d31ba338287f3e3a9b24ac9a26b43f73112
Jermaniac <stalin_bnk@hotmail.com>
2022-01-19 17:32:48 +0100

    added download link cv

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,5 +1,7 @@
 import germanSelfie from "../../assets/german-selfie.jpeg"
+import pdfGerman from "../../assets/CVGERMAN2021-2022.pdf"
 import "./Banner.css"
+import Button from '@mui/material/Button';
 
 function Banner() {
     return (
@@ -9,6 +11,7 @@ function Banner() {
             </picture>
             <h2 className="banner_name">GERMÁN GONZÁLEZ GARZÓN</h2>
             <h3 className="banner_position">Web Developer</h3>
+            <Button className="banner_cv" href={pdfGerman} download="cvgerman" variant="contained">Descargar CV</Button>
         </div>
     );
   }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,13 +4,9 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Badge from '@mui/material/Badge';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import AccountCircle from '@mui/icons-material/AccountCircle';
 import MailIcon from '@mui/icons-material/Mail';
-import NotificationsIcon from '@mui/icons-material/Notifications';
 import MoreIcon from '@mui/icons-material/MoreVert';
 
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
