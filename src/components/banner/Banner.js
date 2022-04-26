a1e1ff4d63c3e68824d30ab22886c49c82ffee54
Jermaniac <stalin_bnk@hotmail.com>
2022-04-27 00:41:31 +0200

    added cards

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -7,7 +7,7 @@ function Banner() {
     return (
         <div className="banner_container">
             <picture className="banner_picture">
-                <img className="banner_img" src={germanSelfie} alt="german-selfie"/>
+                <img className="banner_img" src={germanSelfie} alt="german-selfie" />
             </picture>
             <h2 className="banner_name">GERMÁN GONZÁLEZ GARZÓN</h2>
             <h3 className="banner_position">Software Developer</h3>
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,42 +1,79 @@
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import Link from '@mui/material/Link';
-import moodWebAppGif from '../../assets/moodWebApp.gif'
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import moodWebAppGif from "../../assets/moodWebApp.gif";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
 
 function Projects() {
-    return (
-        <>
-          <Typography
-            variant="h3"
-            component="div"
-            style={{textAlign:"center", padding:"24px"}}
-          >
-            Proyectos
-          </Typography>
+  return (
+    <>
+      <Typography
+        variant="h3"
+        component="div"
+        style={{ textAlign: "center", padding: "24px" }}
+      >
+        Proyectos
+      </Typography>
 
-            <Grid container item xs={12} spacing={2} sx={{padding:"24px"}}>
-                <Grid item xs={12} md={6} sx={{textAlign:"center"}}>
-                    <Typography
-                    variant="h6"
-                    component="div"
-                    style={{textAlign:"center"}}>
-                        Reconocimiento de expresiones faciales
-                    </Typography>
-                    <img style={{maxWidth:"100%"}} src={moodWebAppGif} alt="mood_web_app_gif" />
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <Typography
-                    variant="body1"
-                    component="div"
-                    style={{textAlign:"center"}}>
-                        Se trata de una aplicación que reconoce siete expresiones faciales (enfadado, disgustado, asustado, feliz, triste,
-                        sorprendido, neutral) a partir de la fotografía de un rostro utilizando una red neuronal convolucional o CNN.&nbsp;
-                        <Link href="https://expression-test.vercel.app/">Link proyecto</Link>
-                    </Typography>
-                </Grid>
-            </Grid>
-        </>
-    );
-  }
+      <Grid container spacing={4} style={{ padding: "24px" }}>
+        <Grid item xs={12} md={6}>
+          <Card>
+            <CardMedia
+              component="img"
+              image={moodWebAppGif}
+              alt="mood web app gif"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Reconocimiento de expresiones faciales en web
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Se trata de una web que reconoce siete expresiones faciales
+                (enfadado, disgustado, asustado, feliz, triste, sorprendido,
+                neutral) a partir de la fotografía de un rostro utilizando una
+                red neuronal convolucional o CNN.
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small" href="https://expression-test.vercel.app/">
+                VER PROYECTO
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Card>
+            <CardMedia
+              component="img"
+              image={moodWebAppGif}
+              alt="mood web app gif"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Mood App
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Se trata de una aplicación móvil que reconoce siete expresiones
+                faciales (enfadado, disgustado, asustado, feliz, triste,
+                sorprendido, neutral) a partir de la fotografía de un rostro que
+                puedes escoger de la galería o bien tomarte una foto desde la
+                cámara del dispositivo utilizando una red neuronal convolucional
+                o CNN.
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small" href="https://github.com/gggarzon/TrabajoFinGradoGerman/tree/master/MoodApp">
+                VER PROYECTO
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
+    </>
+  );
+}
 
-  export default Projects;
+export default Projects;
