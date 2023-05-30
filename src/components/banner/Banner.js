f221437c000bba429f0a04189eddeba74949a606
Germán González Garzón <47125376+Jermaniac@users.noreply.github.com>
2023-05-31 00:46:21 +0200

    Update Banner.js

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -7,7 +7,7 @@ import Box from "@mui/material/Box";
 const Banner = () => {
   return (
     <Box
-      className="banner_container"
+      className="banner_containe"
       style={{
         margin: "56px auto",
         textAlign: "center",
