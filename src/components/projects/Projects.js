9a6ff74b1475caeb16c9c3e943ce728464d3fa6a
Jermaniac <stalin_bnk@hotmail.com>
2024-03-13 02:22:04 +0100

    feat - added logos tech and hover cards

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -5,6 +5,7 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
 
 import moodWebDemoVideo from "assets/videos/mood-web-demo.mp4";
 // import moodMobileDemoVideo from "assets/gifs/moodMobileApp.gif";
@@ -21,6 +22,8 @@ import javaLogo from "assets/svg/java.svg";
 import androidLogo from "assets/svg/android.svg";
 
 import { useRef } from "react";
+import { Link } from "@mui/material";
+import { CardActionArea } from "@mui/material";
 
 const Projects = () => {
   const videoRefs = useRef([]);
@@ -56,222 +59,235 @@ const Projects = () => {
         }}
       >
         <Grid item xs={12} lg={6}>
-          <Card sx={{ height: "100%" }}>
-            <CardMedia
-              component="video"
-              loop
-              muted
-              alt="url-shortener-app"
-              src={moodWebDemoVideo}
-              ref={(el) => (videoRefs.current[0] = el)}
-              onMouseEnter={() => handleMouseEnter(0)}
-              onMouseLeave={() => handleMouseLeave(0)}
-            />
-            <CardContent sx={{ px: "36px" }}>
-              <h2>Facial expression recognition web</h2>
-              <p>
-                This project is about an app that recognizes seven facial
-                expressions (angry, disgusted, scared, happy, sad, surprised,
-                neutral) from a face photo using a convolutional neural network
-                trained by me.
-              </p>
-            </CardContent>
-            <CardActions sx={{ justifyContent: "center", pb: "32px" }}>
-              <Button
-                size="small"
-                href="https://expression-test.vercel.app/"
-                variant="contained"
-                color="primary"
-              >
-                <AdsClickIcon sx={{ mr: "6px" }} />
-                CHECK PROJECT
-              </Button>
-            </CardActions>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "end",
-                margin: "24px",
-              }}
-            >
-              <img
-                src={pythonLogo}
-                alt="Python Logo"
-                width={35}
-                height={35}
-                style={{ marginRight: "10px" }}
+          <Card
+            sx={{
+              height: "100%",
+              cursor: "pointer",
+              transition: "transform 0.3s",
+              "&:hover": {
+                transform: "scale(1.02)",
+                boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
+              },
+            }}
+          >
+            <CardActionArea href="https://expression-test.vercel.app/">
+              <CardMedia
+                component="video"
+                loop
+                muted
+                alt="url-shortener-app"
+                src={moodWebDemoVideo}
+                ref={(el) => (videoRefs.current[0] = el)}
+                onMouseEnter={() => handleMouseEnter(0)}
+                onMouseLeave={() => handleMouseLeave(0)}
               />
-              <img
-                src={reactLogo}
-                alt="React Logo"
-                width={35}
-                height={35}
-                style={{ marginRight: "10px" }}
-              />
-              <img
-                src={tailwindLogo}
-                alt="Tailwind Logo"
-                width={35}
-                height={35}
-              />
-            </div>
+              <CardContent sx={{ px: "36px" }}>
+                <h2>Facial expression recognition web</h2>
+                <p>
+                  This project is about an app that recognizes seven facial
+                  expressions (angry, disgusted, scared, happy, sad, surprised,
+                  neutral) from a face photo using a convolutional neural
+                  network trained by me.
+                </p>
+                <Divider />
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "end",
+                    margin: "24px",
+                  }}
+                >
+                  <img
+                    src={pythonLogo}
+                    alt="Python Logo"
+                    width={25}
+                    height={25}
+                    style={{ marginRight: "10px" }}
+                  />
+                  <img
+                    src={reactLogo}
+                    alt="React Logo"
+                    width={25}
+                    height={25}
+                    style={{ marginRight: "10px" }}
+                  />
+                  <img
+                    src={tailwindLogo}
+                    alt="Tailwind Logo"
+                    width={25}
+                    height={25}
+                  />
+                </div>
+              </CardContent>
+            </CardActionArea>
           </Card>
         </Grid>
         <Grid item xs={12} lg={6}>
-          <Card sx={{ height: "100%" }}>
-            <CardMedia
-              component="video"
-              loop
-              muted
-              src={urlShortenerDemoVideo}
-              alt="url-shortener-app"
-              ref={(el) => (videoRefs.current[3] = el)}
-              onMouseEnter={() => handleMouseEnter(3)}
-              onMouseLeave={() => handleMouseLeave(3)}
-            />
-            <CardContent sx={{ px: "36px" }}>
-              <h2>Url Shortener</h2>
-              <p>
-                Introducing the renowned URL shortener project! Simply paste
-                your long URL and receive a concise, shareable link to share
-                with friends. The definitive project!
-              </p>
-            </CardContent>
-            <CardActions sx={{ justifyContent: "center", pb: "32px" }}>
-              <Button
-                size="small"
-                href="https://url-shortener-frontend-xi.vercel.app/"
-                variant="contained"
-                color="primary"
-              >
-                <AdsClickIcon sx={{ mr: "6px" }} />
-                CHECK PROJECT
-              </Button>
-            </CardActions>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "end",
-                margin: "24px",
-              }}
-            >
-              <img
-                src={nodeJsLogo}
-                alt="NodeJs Logo"
-                width={35}
-                height={35}
-                style={{ marginRight: "10px" }}
+          <Card
+            sx={{
+              height: "100%",
+              cursor: "pointer",
+              transition: "transform 0.3s",
+              "&:hover": {
+                transform: "scale(1.02)",
+                boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
+              },
+            }}
+          >
+            <CardActionArea href="https://url-shortener-frontend-xi.vercel.app/">
+              <CardMedia
+                component="video"
+                loop
+                muted
+                src={urlShortenerDemoVideo}
+                alt="url-shortener-app"
+                ref={(el) => (videoRefs.current[3] = el)}
+                onMouseEnter={() => handleMouseEnter(3)}
+                onMouseLeave={() => handleMouseLeave(3)}
               />
-              <img
-                src={astroLogo}
-                alt="Astro Logo"
-                width={35}
-                height={35}
-                style={{ marginRight: "10px" }}
-              />
-              <img
-                src={tailwindLogo}
-                alt="Tailwind Logo"
-                width={35}
-                height={35}
-              />
-            </div>
+              <CardContent sx={{ px: "36px" }}>
+                <h2>Url Shortener</h2>
+                <p>
+                  Introducing the renowned URL shortener project! Simply paste
+                  your long URL and receive a concise, shareable link to share
+                  with friends. The definitive project!
+                </p>
+                <Divider />
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "end",
+                    margin: "24px",
+                  }}
+                >
+                  <img
+                    src={nodeJsLogo}
+                    alt="NodeJs Logo"
+                    width={25}
+                    height={25}
+                    style={{ marginRight: "10px" }}
+                  />
+                  <img
+                    src={astroLogo}
+                    alt="Astro Logo"
+                    width={25}
+                    height={25}
+                    style={{ marginRight: "10px" }}
+                  />
+                  <img
+                    src={tailwindLogo}
+                    alt="Tailwind Logo"
+                    width={25}
+                    height={25}
+                  />
+                </div>
+              </CardContent>
+            </CardActionArea>
           </Card>
         </Grid>
         <Grid item xs={12} lg={6}>
-          <Card sx={{ height: "100%" }}>
-            <CardMedia
-              component="video"
-              loop
-              muted
-              src={moodWebDemoVideo}
-              alt="url-shortener-app"
-              ref={(el) => (videoRefs.current[1] = el)}
-              onMouseEnter={() => handleMouseEnter(1)}
-              onMouseLeave={() => handleMouseLeave(1)}
-            />
-            <CardContent sx={{ px: "36px" }}>
-              <h2>Mood App</h2>
-              <p>
-                This is a mobile application that recognizes seven facial
-                expressions from a face photograph. You can either choose an
-                image from the gallery or take a photo using the device&apos;s
-                camera.
-              </p>
-            </CardContent>
-            <CardActions sx={{ justifyContent: "center", pb: "32px" }}>
-              <Button
-                size="small"
-                href="https://expression-test.vercel.app/"
-                variant="contained"
-                color="primary"
-              >
-                <AdsClickIcon sx={{ mr: "6px" }} />
-                CHECK PROJECT
-              </Button>
-            </CardActions>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "end",
-                margin: "24px",
-              }}
-            >
-              <img
-                src={javaLogo}
-                alt="Java Logo"
-                width={35}
-                height={35}
-                style={{ marginRight: "6px" }}
+          <Card
+            sx={{
+              height: "100%",
+              cursor: "pointer",
+              transition: "transform 0.3s",
+              "&:hover": {
+                transform: "scale(1.02)",
+                boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
+              },
+            }}
+          >
+            <CardActionArea href="https://expression-test.vercel.app/">
+              <CardMedia
+                component="video"
+                loop
+                muted
+                src={moodWebDemoVideo}
+                alt="url-shortener-app"
+                ref={(el) => (videoRefs.current[1] = el)}
+                onMouseEnter={() => handleMouseEnter(1)}
+                onMouseLeave={() => handleMouseLeave(1)}
               />
-              <img
-                src={androidLogo}
-                alt="Android Logo"
-                width={35}
-                height={35}
-              />
-            </div>
+              <CardContent sx={{ px: "36px" }}>
+                <h2>Mood App</h2>
+                <p>
+                  This is a mobile application that recognizes seven facial
+                  expressions from a face photograph. You can either choose an
+                  image from the gallery or take a photo using the device&apos;s
+                  camera.
+                </p>
+                <Divider />
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "end",
+                    margin: "12px",
+                  }}
+                >
+                  <img
+                    src={javaLogo}
+                    alt="Java Logo"
+                    width={25}
+                    height={25}
+                    style={{ marginRight: "6px" }}
+                  />
+                  <img
+                    src={androidLogo}
+                    alt="Android Logo"
+                    width={25}
+                    height={25}
+                  />
+                </div>
+              </CardContent>
+            </CardActionArea>
           </Card>
         </Grid>
         <Grid item xs={12} lg={6}>
-          <Card sx={{ height: "100%" }}>
-            <CardMedia
-              component="video"
-              loop
-              muted
-              src={pricingCryptoDemoVideo}
-              alt="url-shortener-app"
-              ref={(el) => (videoRefs.current[2] = el)}
-              onMouseEnter={() => handleMouseEnter(2)}
-              onMouseLeave={() => handleMouseLeave(2)}
-            />
-            <CardContent sx={{ px: "36px" }}>
-              <h2>Crypto Pricing web</h2>
-              <p>
-                It&apos;s a very simple web that displays real-time prices for
-                10 cryptocurrencies. The data used belongs to the Messari API.
-              </p>
-            </CardContent>
-            <CardActions sx={{ justifyContent: "center", pb: "32px" }}>
-              <Button
-                size="small"
-                href="https://test-ms.vercel.app/"
-                variant="contained"
-                color="primary"
-              >
-                <AdsClickIcon sx={{ mr: "6px" }} />
-                CHECK PROJECT
-              </Button>
-            </CardActions>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "end",
-                margin: "24px",
-              }}
-            >
-              <img src={reactLogo} alt="React Logo" width={35} height={35} />
-            </div>
+          <Card
+            sx={{
+              height: "100%",
+              cursor: "pointer",
+              transition: "transform 0.3s",
+              "&:hover": {
+                transform: "scale(1.02)",
+                boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
+              },
+            }}
+          >
+            <CardActionArea href="https://test-ms.vercel.app/">
+              <CardMedia
+                component="video"
+                loop
+                muted
+                src={pricingCryptoDemoVideo}
+                alt="url-shortener-app"
+                ref={(el) => (videoRefs.current[2] = el)}
+                onMouseEnter={() => handleMouseEnter(2)}
+                onMouseLeave={() => handleMouseLeave(2)}
+              />
+              <CardContent sx={{ px: "36px" }}>
+                <h2>Crypto Pricing web</h2>
+                <p>
+                  It&apos;s a very simple web that displays real-time prices for
+                  10 cryptocurrencies. The data used belongs to the Messari API.
+                </p>
+                <Divider />
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "end",
+                    margin: "24px",
+                  }}
+                >
+                  <img
+                    src={reactLogo}
+                    alt="React Logo"
+                    width={25}
+                    height={25}
+                  />
+                </div>
+              </CardContent>
+            </CardActionArea>
           </Card>
         </Grid>
       </Grid>
